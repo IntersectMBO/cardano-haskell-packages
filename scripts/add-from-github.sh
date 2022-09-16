8507f576b7a58854aca7ee87de501c75ec9d5e6b
Andrea Bedini <andrea@andreabedini.com>
2022-09-16 11:27:06 +0200

    add-from-github script can force versions (#26)
    * add-from-github script can force versions
    
    Closes #18
    
    * Properly check if we are pointed to a single package

--- scripts/add-from-github.sh
+++ scripts/add-from-github.sh
@@ -3,25 +3,75 @@
 set -o errexit
 set -o pipefail
 
-REPO_URL="$1" # e.g. https://github.com/input-output-hk/optparse-applicative
-REV="$2"  # e.g. 7497a29cb998721a9068d5725d49461f2bba0e7a
+function usage {
+  echo "Usage $(basename "$0") [-r REVISION] [-v VERSION] REPO_URL REV [SUBDIRS...]"
+  echo
+  echo "        -r REVISION     adds .0.0.0.0.REVISION to the package version"
+  echo "        -v VERSION      uses VERSION as the package version"
+  exit
+}
 
-if ! shift 2; then
-  echo "Usage: $0 REPO_URL REV [SUBDIR...]"
+optstring=":hr:v:"
+
+REVISION=
+VERSION=
+
+while getopts ${optstring} arg; do
+  case ${arg} in
+    h)
+      usage
+      ;;
+    r)
+      REVISION="${OPTARG}"
+      ;;
+    v)
+      VERSION="${OPTARG}"
+      ;;
+    :)
+      echo "$0: Must supply an argument to -$OPTARG." >&2
+      exit 1
+      ;;
+    ?)
+      echo "Invalid option: -${OPTARG}."
+      exit 2
+      ;;
+  esac
+done
+
+if [[ -n $VERSION && -n $REVISION ]]; then
+  echo "You can either use -r or -v but not both at the same time"
   exit 1
 fi
 
-SUBDIRS="$*"
+shift $((OPTIND - 1))
+
+REPO_URL="$1" # e.g. https://github.com/input-output-hk/optparse-applicative
+REPO_REV="$2"  # e.g. 7497a29cb998721a9068d5725d49461f2bba0e7a
+
+if ! shift 2; then
+  usage
+fi
+
+SUBDIRS=("$@")
+
+if [[ ${#SUBDIRS[@]} -gt 1 && (-n $VERSION || -n $REVISION) ]]; then
+  echo "You can use -r or -v only with a single package"
+  exit 1
+fi
 
 render_meta() {
   local TIMESTAMP=$1
   local URL=$2
   local SUBDIR=$3
+  local FORCE_VERSION=$4
 
   echo "timestamp = $TIMESTAMP"
   echo "url = '$URL'"
   if [[ -n $SUBDIR ]]; then
     echo "subdir = '$SUBDIR'"
+  fi
+  if [[ -n $FORCE_VERSION ]]; then
+    echo "force-version = true"
   fi
 }
 
@@ -47,6 +97,12 @@ do_package() {
   local PKG_VERSION
   PKG_VERSION=$(awk -v IGNORECASE=1 -e '/^version/ { print $2 }' "$CABAL_FILE")
 
+  if [[ -n $VERSION ]]; then
+    PKG_VERSION="$VERSION"
+  elif [[ -n $REVISION ]]; then
+    PKG_VERSION="${PKG_VERSION}.0.0.0.0.${REVISION}"
+  fi
+
   local PKG_ID="$PKG_NAME-$PKG_VERSION"
 
   if [[ -z $PKG_VERSION ]]; then
@@ -66,34 +122,34 @@ do_package() {
   fi
 
   mkdir -p "$(dirname "$METAFILE")"
-  render_meta "$TIMESTAMP" "$URL" "$SUBDIR" > "$METAFILE"
+  render_meta "$TIMESTAMP" "$URL" "$SUBDIR" "$REVISION$VERSION" > "$METAFILE"
   log "Written $METAFILE"
 
   git add "$METAFILE"
-  git commit -m"Added $PKG_ID" -m "From $REPO_URL at $REV"
+  git commit -m"Added $PKG_ID" -m "From $REPO_URL at $REPO_REV"
 }
 
-TAR_URL="$REPO_URL/tarball/$REV"
+TAR_URL="$REPO_URL/tarball/$REPO_REV"
 TIMESTAMP=$(date --utc +%Y-%m-%dT%H:%M:%SZ)
 
 WORKDIR=$(mktemp -d)
 log "Work directory is $WORKDIR"
 pushd "$WORKDIR"
 
-log "Fetching $REPO_URL at revision $REV"
+log "Fetching $REPO_URL at revision $REPO_REV"
 
 if ! curl --fail --silent --location --remote-name "$TAR_URL"; then
   echo "Failed to download $TAR_URL"
   exit 1
 fi
 
-tar xzf * --strip-component=1 --wildcards '**/*.cabal'
+tar xzf ./* --strip-component=1 --wildcards '**/*.cabal'
 popd
 
-if [[ -z $SUBDIRS ]]; then
+if [[ ${#SUBDIRS[@]} -eq 0 ]]; then
   do_package "$TAR_URL" "" "$WORKDIR"
 else
-  for subdir in $SUBDIRS; do
+  for subdir in "${SUBDIRS[@]}"; do
     do_package "$TAR_URL" "$subdir" "$WORKDIR"
   done
 fi
