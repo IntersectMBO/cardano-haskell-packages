31e02225283cc4624b3e7873a397fd53b2599f49
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2022-11-30 11:41:56 +0000

    Add a builder for packages using Nix, set cardano-node as a smoke test
    This adds the infrastructure to build packages from CHaP with Nix in our
    CI. There is a decent amount of ugliness but it works.
    
    At the moment it doesn't try to be particularly clever: you can build a
    single package-version, or a bunch of them (better for parallelism!), and
    it's up to you to tell it what to build. But that's relatively easy to
    do externally once the nix stuff is in place.

--- flake.nix
+++ flake.nix
@@ -2,16 +2,65 @@
   description = "Metadata for Cardano's Haskell package repository";
 
   inputs = {
-    nixpkgs.follows = "foliage/nixpkgs";
-    foliage.url = "github:andreabedini/foliage";
+    nixpkgs.follows = "haskell-nix/nixpkgs";
+    foliage = {
+      url = "github:andreabedini/foliage";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
+    haskell-nix = {
+      url = "github:input-output-hk/haskell.nix";
+      inputs.hackage.follows = "hackage";
+    };
+    hackage = { 
+      url = "github:input-output-hk/hackage.nix";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs, foliage, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
+  outputs = { self, nixpkgs, foliage, flake-utils, haskell-nix, ... }:
+    let ghcVersions = [ "ghc8107" "ghc924" ];
+    in flake-utils.lib.eachDefaultSystem
       (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
+        let overlays = [ haskell-nix.overlay ];
+            pkgs = import nixpkgs { inherit system overlays; inherit (haskell-nix) config; };
+            foliageExe = foliage.packages.${system}.default;
+            nix-tools = pkgs.haskell-nix.nix-tools.ghc8107;
+
+            package-project = { built-repo, compiler-nix-name, pkgname, version }:
+              pkgs.haskell-nix.cabalProject' {
+                src = ./.;
+                inherit compiler-nix-name;
+                inputMap = { "https://input-output-hk.github.io/cardano-haskell-packages" = built-repo; };
+                # Note: tests and benchmarks are _not_ enabled, but exectuables are
+                cabalProjectLocal = ''
+                  extra-packages: ${pkgname}
+                  constraints: ${pkgname} == ${version}
+                '';
+              };
+            package-derivations = args@{ built-repo, compiler-nix-name,  pkgname,  version }:
+              let hsPkg = (package-project args).hsPkgs.${pkgname};
+              in pkgs.haskell-nix.haskellLib.getAllComponents hsPkg;
+
+            # Job for building all components of a single package version. You have to
+            # build the cabal repository yourself and pass it as an argument, since
+            # building it is impure and we don't want to pin its sha here, as it will
+            # change over time.
+            build-pkg-job = args@{ built-repo, compiler-nix-name, pkgname, version }:
+              let test-drvs = package-derivations args;
+              in pkgs.releaseTools.aggregate {
+                name = "${compiler-nix-name}-${pkgname}-${version}";
+                constituents = test-drvs;
+              };
+            # Job for building a bunch of build-pkg-jobs together
+            build-pkgs-job = pkg-args:
+              let test-jobs = builtins.map build-pkg-job pkg-args;
+              in pkgs.releaseTools.aggregate {
+                name = "multi-package-job";
+                constituents = test-jobs;
+              };
+        in {
+          functions = { inherit build-pkg-job build-pkgs-job; };
           devShells.default = with pkgs; mkShellNoCC {
             name = "cardano-haskell-packages";
             buildInputs = [
@@ -20,7 +69,8 @@
               curlMinimal.bin
               gitMinimal
               gnutar
-              foliage.packages.${system}.default
+              foliageExe
+              nix-tools
             ];
           };
         });
---
+++ scripts/test-package-build.sh
@@ -0,0 +1,12 @@
+# Run from the root
+
+BUILT_REPO=$1
+COMPILER_NIX_NAME=$2
+PKGNAME=$3
+VERSION=$4
+
+# TODO: nicer interface, easier to test lots of different packages
+
+# This ugliness is apparently the best way to run a function from a flake today,
+# taken from https://github.com/NixOS/nix/issues/5663#issuecomment-1278505018
+nix build --impure --expr "(builtins.getFlake ''git+file://$PWD'').functions.x86_64-linux.build-pkg-job { built-repo = $BUILT_REPO; compiler-nix-name = ''$COMPILER_NIX_NAME''; pkgname = ''$PKGNAME''; version = ''$VERSION''; }"
