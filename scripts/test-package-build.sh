04f8c04e71c7b79a7a8853da177b7065beb8e266
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2023-02-10 13:45:45 +0000

    Allow shallow checkout

--- scripts/test-package-build.sh
+++ scripts/test-package-build.sh
@@ -9,4 +9,4 @@ VERSION=$4
 
 # This ugliness is apparently the best way to run a function from a flake today,
 # taken from https://github.com/NixOS/nix/issues/5663#issuecomment-1278505018
-nix build --impure --expr "(builtins.getFlake ''git+file://$PWD'').functions.x86_64-linux.build-pkg-job { built-repo = $BUILT_REPO; compiler-nix-name = ''$COMPILER_NIX_NAME''; pkgname = ''$PKGNAME''; version = ''$VERSION''; }"
+nix build --impure --expr "(builtins.getFlake ''git+file://$PWD?shallow=1'').functions.x86_64-linux.build-pkg-job { built-repo = $BUILT_REPO; compiler-nix-name = ''$COMPILER_NIX_NAME''; pkgname = ''$PKGNAME''; version = ''$VERSION''; }"
