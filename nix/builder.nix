0f09e11036a583e0ff890fea449826a4ad043d84
Andrea Bedini <andrea.bedini@tweag.io>
2023-05-16 14:16:32 +0800

    Add more comments

--- nix/builder.nix
+++ nix/builder.nix
@@ -16,6 +16,14 @@ let
     let
       package-id = "${package-name}-${package-version}";
 
+      # Global config needed to build CHaP packages should go here. Obviously
+      # this should be kept to an absolute minimum, since that means config
+      # that every downstream project needs also.
+      #
+      # No need to set index-state:
+      # - haskell.nix will automatically use the latest known one for hackage
+      # - we want the very latest state for CHaP so it includes anything from
+      #   e.g. a PR being tested
       project = (pkgs.haskell-nix.cabalProject' {
         inherit compiler-nix-name;
 
@@ -42,6 +50,19 @@ let
         }];
       });
 
+      # Wrapper around all package components
+      #
+      # The wrapper also provides shortcuts to quickly manipulate the cabal project.
+      #
+      # - addCabalProject adds arbitrary configuration to the project's cabalProjectLocal
+      # - addConstraint uses addCabalProject to add a "constraints: " stanza
+      # - allowNewer uses addCabalProject to add a "allow-newer: " stanza
+      #
+      # Note 1: We use cabalProjectLocal to be able to override cabalProject
+      # Note 2: `cabalProjectLocal` ends up being prepended to the existing one
+      # rather than appended. I think this is haskell.nix bug. If the project
+      # has already a `cabalProjectLocal` this might not give the intended
+      # result.
       aggregate = project:
         pkgs.releaseTools.aggregate
           {
@@ -52,11 +73,9 @@ let
             constituents = haskellLib.getAllComponents project.hsPkgs.${package-name};
           } // {
           passthru = {
+            # pass through the project for debugging purposes
             inherit project;
-            # We use cabalProjectLocal to be able to override cabalProject
-            # Note: `cabalProjectLocal` ends up being prepended to the
-            # existing one rather than appended. If the project has already
-            # a `cabalProjectLocal` this might not give the intended result
+            # Shortcuts to manipulate the project, see above
             addCabalProject = cabalProjectLocal: aggregate (
               project.appendModule { inherit cabalProjectLocal; }
             );
