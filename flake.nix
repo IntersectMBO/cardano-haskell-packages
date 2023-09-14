1d36c67a2914ed7489c4c5b640d3e3d776776e99
Jamie Bertram <jhbertra@gmail.com>
2023-09-14 13:37:19 -0400

    Add marlowe-plutus-0.1.0.0 (#499)
    * Add marlowe-runtime to smoke tests
    
    Rationale: marlowe-runtime is an excellent smoke test. It is fairly far
    downstream, and it depends on several other packages in the registry. As
    such, it breaks easily if upstream packages fail to specify their
    dependency version constraints accurately. Additionally, it is a
    significant amount of work to fix the build each time new marlowe
    packages are released, because updates to other packages frequently break
    it in the meantime, so keeping it always building will make this much
    easier.
    
    * Added marlowe-plutus-0.1.0.0
    
    From https://github.com/input-output-hk/marlowe-plutus at b7639be1239a589f8965828519c97542f60ecf99
    
    * Update codeowners and exclude GHC9 for marlowe-plutus
    
    * Remove marlowe-runtime smoke test package

--- flake.nix
+++ flake.nix
@@ -106,6 +106,10 @@
           ghc92.enabled = false;
           ghc96.enabled = false;
         };
+        marlowe-plutus = {
+          ghc92.enabled = false;
+          ghc96.enabled = false;
+        };
         quickcheck-contractmodel = {
           ghc92.enabled = false;
           ghc96.enabled = false;
