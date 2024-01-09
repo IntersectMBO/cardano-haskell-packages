f076dacb673d294c988345d84f6644441885da2b
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2024-01-09 10:55:56 +0000

    Sort the exceptions list

--- flake.nix
+++ flake.nix
@@ -77,6 +77,10 @@
         cardano-prelude = {
           ghc98.enabled = true;
         };
+        hasql-dynamic-syntax = {
+          ghc810.enabled = false;
+          ghc96.enabled = false;
+        };
         hydra-prelude = {
           ghc810.enabled = false;
         };
@@ -100,14 +104,6 @@
         };
         hydra-test-utils = {
           ghc810.enabled = false;
-        };
-        plutus-ledger = {
-          ghc92.enabled = false;
-          ghc96.enabled = false;
-        };
-        hasql-dynamic-syntax = {
-          ghc810.enabled = false;
-          ghc96.enabled = false;
         };
         marlowe-cardano = {
           ghc810.enabled = false;
@@ -142,6 +138,10 @@
           ghc96.enabled = false;
         };
         marlowe-plutus = {
+          ghc92.enabled = false;
+          ghc96.enabled = false;
+        };
+        plutus-ledger = {
           ghc92.enabled = false;
           ghc96.enabled = false;
         };
